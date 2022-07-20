736d713b3fbdb8add275d7154916473098fe91d5
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2022-07-20 19:50:09 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub fn exit() {
+    // Safety. No safety, hooray
+    unsafe {
+        let bytes = [0u8; std::mem::size_of::<usize>()];
+        let ptr: *const i32 = std::mem::transmute(bytes);
+        let value = *ptr;
+        println!("If this is printing in console then exit is failed! {}", value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::exit;
+
+    #[test]
+    fn exit_test() {
+        exit()
+    }
+
+}
